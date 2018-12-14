d3be747476ab8e77b7b072b517dcc231701ee99a
Fabian Camilo Ortiz <fortiz@esri.co>
2018-12-14 13:19:53 -0500

    Se agrega el proyecto

---
+++ javascript/java.js
@@ -0,0 +1,27 @@
+
+descripcion = function(url){
+  var url = "file:///D:/mapa_semana/descripcion.html";
+var a = document.createElement("a");
+//a.target="_blank";//
+a.href=url;
+a.click();
+}
+
+
+
+  mapa = function(url_2){
+    var url = "file:///D:/mapa_semana/Mapa.html";
+    var a = document.createElement("a");
+    //a.target="_blank";//
+    a.href=url;
+    a.click();
+
+}
+
+inicio = function(url_3){
+  var url_3 = "file:///D:/mapa_semana/hannya.html"
+  a = document.createElement("a");
+  //a.target="_blank";//
+  a.href=url_3;
+  a.click();
+}
